a81dbce3f25697b483872ca2f0dbc04f484372af
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-11 13:33:01 +0800

    Add default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      pink: 'hsl(275, 100%, 97%)',
+      purpleLight: 'hsl(292, 16%, 49%)',
+      purpleDark: 'hsl(292, 42%, 14%)',
+    },
+    fontFamily: {
+      workSans: ["'Work Sans'", 'sans-serif'],
+    },
   },
   plugins: [],
 };
+
+/*
+- Mobile: 375px
+- Desktop: 1440px
+*/
